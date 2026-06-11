b36f6b71b0f19e093836a509a5ed10c0f8c9183a
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-06-12 02:40:56 +0530

    revision done of two topics

--- Arrays/rev.cpp
+++ Arrays/rev.cpp
@@ -3,19 +3,31 @@
 #include<iostream>
 using namespace std;
 
-// Topic- Finding Largest Element 
-void Largest(int arr[],int n){
-    int  Biggest = INT_MIN;
-    for(int i = 0; i<n;i++){
-        Biggest = max(Biggest,arr[i]);
+void PrintArr(int arr[],int n){
+    for(int i =0;i<n;i++){
+        cout<<arr[i]<<" ";
     }
-    cout<<"The Largest Number of this array is " << Biggest;
 }
+// Topic- Finding Largest Element 
+// void Largest(int arr[],int n){
+//     int  Biggest = INT_MIN;
+//     for(int i = 0; i<n;i++){
+//         Biggest = max(Biggest,arr[i]);
+//     }
+//     cout<<"The Largest Number of this array is " << Biggest;
+// }
 
+// Reverse Array
+void ReverseArray(int arr[],int n){
+    for(int i = 0 ; i< (n/2) ;i++){
+        swap(arr[i],arr[n-1-i]);
+    }
+    PrintArr(arr,n);
+}
 int main(){
-    int nums[] = {7, 2, 11, 4, 9};
+    int nums[] = {1, 2, 3, 4, 5, 6};
     int n = sizeof(nums) / sizeof(int);
-    Largest(nums,n);
+    ReverseArray(nums,n);
     return 0;
 }
 

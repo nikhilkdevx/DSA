fd8784984b6c3a934568f33dd1ced3c2de510611
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-06-14 00:38:51 +0530

    today rev done

--- Arrays/rev.cpp
+++ Arrays/rev.cpp
@@ -18,16 +18,49 @@ void PrintArr(int arr[],int n){
 // }
 
 // Reverse Array
-void ReverseArray(int arr[],int n){
-    for(int i = 0 ; i< (n/2) ;i++){
-        swap(arr[i],arr[n-1-i]);
-    }
-    PrintArr(arr,n);
+// void ReverseArray(int arr[],int n){
+//     for(int i = 0 ; i< (n/2) ;i++){
+//         swap(arr[i],arr[n-1-i]);
+//     }
+//     PrintArr(arr,n);
+// }
+
+// Topic - Linear Search
+
+// int Linear(int arr[], int n , int key){
+//     for(int i = 0; i<n; i++){
+//         if(arr[i]== key){
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
+
+// Topic BinarySearch
+
+int BinarySearch(int arr[] , int n , int key){
+        int st = 0;
+        int end = n-1;
+        while(st <= end){
+            int mid = (st + end) / 2;
+            if(arr[mid]== key){
+                return mid;
+            } else if(arr[mid]>key){
+                end = mid -1;
+            } else{
+                st = mid + 1;
+            }
+        }
+
+    return -1;
+   
 }
+
 int main(){
-    int nums[] = {1, 2, 3, 4, 5, 6};
+    int nums[] = {1 , 3 , 5 ,7, 9 ,11 ,13};
     int n = sizeof(nums) / sizeof(int);
-    ReverseArray(nums,n);
+    int target = 11;
+    cout<<BinarySearch(nums , n , target);
     return 0;
 }
 

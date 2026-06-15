520d5cd435aaf908d463a357a2507c7ffb5f6338
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-06-16 03:22:36 +0530

    solved dsa questions

---
+++ Charcater/assignment.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+using namespace std;
+
+// 1
+
+// void Vowel(string str  , int n ){
+//     int count = 0;
+//     for(int i =0;i<n;i++){
+//         if( str[i] =='a'||
+//             str[i] =='e' ||
+//             str[i] =='i' || 
+//             str[i] =='o' || 
+//             str[i] =='u'){
+//             count++;
+//         }
+//     }
+//     cout << count;
+
+// }
+
+// 2
+
+void Sorting(string s, string t){
+    
+}
+
+int main(){
+    string str1 = "bank";
+    string str2 = "kanb";
+    
+    
+
+    return 0;
+}

e0ddb8c2e918467f5fac9e83900a00a36f63c384
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-06-27 08:21:29 +0530

    Revise Rainwater Problem

--- Arrays/rev.cpp
+++ Arrays/rev.cpp
@@ -3,11 +3,14 @@
 #include<iostream>
 using namespace std;
 
+// For Printing Array
+
 void PrintArr(int arr[],int n){
     for(int i =0;i<n;i++){
-        cout<<arr[i]<<" ";
+        cout<<arr[i]<<",";
     }
 }
+
 // Topic- Finding Largest Element 
 // void Largest(int arr[],int n){
 //     int  Biggest = INT_MIN;
@@ -38,29 +41,91 @@ void PrintArr(int arr[],int n){
 
 // Topic BinarySearch
 
-int BinarySearch(int arr[] , int n , int key){
-        int st = 0;
-        int end = n-1;
-        while(st <= end){
-            int mid = (st + end) / 2;
-            if(arr[mid]== key){
-                return mid;
-            } else if(arr[mid]>key){
-                end = mid -1;
-            } else{
-                st = mid + 1;
-            }
-        }
+// int BinarySearch(int arr[] , int n , int key){
+//         int st = 0;
+//         int end = n-1;
+//         while(st <= end){
+//             int mid = (st + end) / 2;
+//             if(arr[mid]== key){
+//                 return mid;
+//             } else if(arr[mid]>key){
+//                 end = mid -1;
+//             } else{
+//                 st = mid + 1;
+//             }
+//         }
 
-    return -1;
+//     return -1;
    
+// }
+
+// Topic Maximum Subarray Sum
+
+// void Subarray(int arr[],int n){
+//     int maxSum = INT_MIN;
+//     int sum = 0;
+//     for(int i =0;i<n;i++){
+        
+//         sum += arr[i];  
+//         maxSum = max(maxSum,sum);
+//         if(sum<0){
+//             sum = 0;
+//         }
+        
+//     }
+//     cout << "The maximum sum of this array is " <<maxSum;
+// }
+
+// Stocks Buy and Sell
+
+// void Stocks(int arr[],int n){
+    
+//     int minPrice = arr[0];
+    
+//     int MaxProfit = 0;
+
+//     for(int i=1;i<n;i++){
+//         minPrice = min(minPrice,arr[i]);
+//         int Profit = arr[i] - minPrice;
+//         MaxProfit = max(MaxProfit,Profit);
+//     }
+
+//     cout << " The Maximum Profit can be "<< MaxProfit;
+// }
+
+// Topic - Rainwater
+
+void Rainwater(int arr[],int n){
+    int LeftMax[10000] , RightMax[10000];
+    int trapped[10000] = {0};
+    LeftMax[0] = arr[0];
+    RightMax[n-1] = arr[n-1];
+    int water = 0;
+    
+    for(int i = 1;i<n;i++){
+        LeftMax[i] = max(arr[i],LeftMax[i-1]);
+
+    }
+    
+    for(int i = n-2; i>=0 ; i--){
+        RightMax[i] = max(arr[i],RightMax[i+1]);
+    }
+
+    for(int i = 1; i<n-1;i++){
+        trapped[i] = min(LeftMax[i],RightMax[i]) - arr[i];
+        water += max(0, trapped[i]);
+    }
+    
+    cout << "The maximum water Stored is "<< water;
+    
+    
 }
 
+
 int main(){
-    int nums[] = {1 , 3 , 5 ,7, 9 ,11 ,13};
-    int n = sizeof(nums) / sizeof(int);
-    int target = 11;
-    cout<<BinarySearch(nums , n , target);
+    int height[] = {4,2,0,6,3,2,5};
+    int n = sizeof(height) / sizeof(int);
+    Rainwater(height,n);
     return 0;
 }
 

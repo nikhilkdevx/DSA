adcde52a949b9dea71de2a2a549b5419b04145f6
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-08-17 16:29:51 +0530

    sudoku solver revision

--- Backtracking/assign.cpp
+++ Backtracking/assign.cpp
@@ -4,33 +4,32 @@
 using namespace std;
 
 void printSudoku(int sudoku[9][9]){
-    for(int i=0;i<9;i++){
-        for(int j=0;j<9;j++){
+    for(int i = 0;i<9;i++){
+        for(int j = 0;j<9;j++){
             cout << sudoku[i][j] << " ";
         }
         cout << endl;
     }
-}
+};
 
 bool isSafe(int sudoku[9][9],int row,int col,int digit){
-    for(int i=0;i<=8;i++){
+    for(int i = 0;i<9;i++){
         if(sudoku[i][col] == digit){
             return false;
         }
     }
 
-    for(int j = 0;j<=8;j++){
+    for(int j = 0;j<9;j++){
         if(sudoku[row][j] == digit){
             return false;
         }
     }
 
-    // 3*3 grid
     int startRow = (row/3) * 3;
     int startCol = (col/3) * 3;
 
-    for(int i = startRow; i<= startRow + 2;i++){
-        for(int j = startCol; j <= startCol + 2; j++){
+    for(int i = startRow; i<= startRow+2;i++){
+        for(int j = startCol; j<= startCol+2;j++){
             if(sudoku[i][j] == digit){
                 return false;
             }
@@ -38,35 +37,39 @@ bool isSafe(int sudoku[9][9],int row,int col,int digit){
     }
 
     return true;
-}
+
+};
 
 bool sudokuSolver(int sudoku[9][9],int row,int col){
     if(row == 9){
         printSudoku(sudoku);
         return true;
     }
+
     int nextRow = row;
     int nextCol = col + 1;
-    if(col + 1 == 9){
+
+    if(col+1 == 9){
         nextRow = row + 1;
         nextCol = 0;
     }
+
     if(sudoku[row][col] != 0){
         return sudokuSolver(sudoku,nextRow,nextCol);
     }
 
-    for(int digit = 1;digit <= 9;digit++){
+    for(int digit = 1;digit<=9;digit++){
         if(isSafe(sudoku,row,col,digit)){
             sudoku[row][col] = digit;
             if(sudokuSolver(sudoku,nextRow,nextCol)){
                 return true;
             }
-           sudoku[row][col] = 0;
+            sudoku[row][col] = 0;
         }
     }
 
     return false;
-}
+};
 
 int main(){
     int sudoku[9][9] = {{0,0,8,0,0,0,0,0,0},

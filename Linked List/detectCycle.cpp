1667f805d7f159e7681812baa733954c27ced11c
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-08-19 06:44:11 +0530

    Leant merge Sort on LinkedList

--- Linked List/detectCycle.cpp
+++ Linked List/detectCycle.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<list>
 using namespace std;
 
 class Node{
@@ -46,6 +47,16 @@ class List{
         head = head->next;
         temp->next = NULL;
         delete temp;
+    }
+
+    void push_back(int val){
+        Node* newNode = new Node(val);
+        if(head == NULL){
+            head = tail = newNode;
+        } else{
+            tail->next = newNode;
+            tail = newNode;
+        }
     }
 };
 
@@ -115,14 +126,72 @@ void removeCycle(Node* head){
 
 }
 
+Node* splitAtMid(Node* head){
+    Node* slow = head;
+    Node* fast = head;
+    Node* prev = NULL;
+    while(fast != NULL && fast->next != NULL){
+        prev = slow;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    if(prev != NULL){
+        prev->next = NULL;
+    }
+
+    return slow;
+};
+
+Node* merge(Node* left,Node* right){
+    List ans;
+    Node* i = left;
+    Node* j = right;
+
+    while(i != NULL && j != NULL){
+        if(i->data <= j->data){
+            ans.push_back(i->data);
+            i = i ->next;
+        } else {
+            ans.push_back(j->data);
+            j = j->next;
+        }
+
+    } 
+
+    while(i != NULL){
+        ans.push_back(i->data);
+        i = i ->next;
+    }
+
+    while(j != NULL){
+        ans.push_back(j->data);
+        j = j ->next;
+    }
+    return ans.head;
+};
+
+Node* mergeSort(Node* head){
+    if(head == NULL || head->next == NULL){
+        return head;
+    }
+    Node* righthead = splitAtMid(head);
+
+    Node* left = mergeSort(head);
+    Node* right = mergeSort(righthead);
+
+    return merge(left,right);
+};
+
 int main(){
     List ll;
-    ll.push_front(4);
-    ll.push_front(3);
-    ll.push_front(2);
     ll.push_front(1);
-    ll.tail->next = ll.head;
-    removeCycle(ll.head);
+    ll.push_front(2);
+    ll.push_front(3);
+    ll.push_front(4);
+    ll.push_front(5);
+    printList(ll.head);
+    ll.head = mergeSort(ll.head);
     printList(ll.head);
     return 0;
 }

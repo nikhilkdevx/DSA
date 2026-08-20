9f52ee1ef2d58bc71e13450f46e1d0fdf915d25a
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-08-20 15:41:33 +0530

    Leant about zigZag LL

--- Linked List/detectCycle.cpp
+++ Linked List/detectCycle.cpp
@@ -183,15 +183,61 @@ Node* mergeSort(Node* head){
     return merge(left,right);
 };
 
+Node* reverse(Node* head){
+    Node* prev = NULL;
+    Node* curr = head;
+    Node* next = NULL;
+    
+    while(curr != NULL){
+        next = curr->next;
+        curr->next = prev;
+
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+
+}
+
+
+Node* zigZag(Node* head){
+    Node* rightHead =  splitAtMid(head);
+    Node* rightHeadRev = reverse(rightHead);
+
+    // Alternate Merging
+    Node* left = head;
+    Node* right = rightHeadRev;
+    Node* tail = right;
+
+    while (left != NULL && right != NULL)
+    {
+        Node* nextLeft = left->next;
+        Node* nextRight = right->next;
+
+        left->next = right;
+        right->next = nextLeft;
+        tail = right;
+
+        left = nextLeft;
+        right = nextRight;
+    }
+    
+    if(right != NULL){
+        tail->next = right;
+    }
+
+    return head;
+}
+
 int main(){
     List ll;
-    ll.push_front(1);
-    ll.push_front(2);
-    ll.push_front(3);
-    ll.push_front(4);
     ll.push_front(5);
+    ll.push_front(4);
+    ll.push_front(3);
+    ll.push_front(2);
+    ll.push_front(1);
     printList(ll.head);
-    ll.head = mergeSort(ll.head);
+    ll.head = zigZag(ll.head);
     printList(ll.head);
     return 0;
 }

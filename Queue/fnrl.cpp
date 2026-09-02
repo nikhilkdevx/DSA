db0e3d8ca95510e7355d6117f6dd8cc135bed96a
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-09-02 07:17:32 +0530

    learnt about two topics in queues

---
+++ Queue/fnrl.cpp
@@ -0,0 +1,32 @@
+// First non Repeating Letters
+#include<iostream>
+#include<queue>
+using namespace std;
+
+void FirstNonRepeating(string str){
+    queue<char> q;
+    int freq[26] = {0};
+
+    for(int i = 0;i<str.size();i++){
+        char ch = str[i];
+        q.push(ch);
+        freq[ch - 'a']++;
+
+        while(!q.empty() && freq[q.front() - 'a'] > 1){
+            q.pop();
+        };
+
+        if(q.empty()){
+            cout << "-1\n";
+        } else{
+            cout << q.front() << "\n";
+        }
+    };
+}
+
+int main(){
+    string str = "aabccxb";
+    FirstNonRepeating(str);
+    return 0;
+}
+

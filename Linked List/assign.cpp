3cc5888c1e5ef0dac4d133966fa2c147999cd750
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-09-13 19:07:01 +0530

    solved one question

--- Linked List/assign.cpp
+++ Linked List/assign.cpp
@@ -1,17 +1,19 @@
-#include<iostream>
-#include<list>
+#include <iostream>
+#include <list>
 using namespace std;
 
-class Node{
-public:
-    int data;
-    Node* next;
+// class Node
+// {
+// public:
+//     int data;
+//     Node *next;
 
-    Node(int val){
-        data = val;
-        next = NULL;
-    }
-};
+//     Node(int val)
+//     {
+//         data = val;
+//         next = NULL;
+//     }
+// };
 // brute Force
 // bool intersection(Node* head1,Node* head2){
 //     Node* temp1 = head1;
@@ -26,45 +28,88 @@ public:
 //         }
 //         temp2 = head2;
 //         temp1 = temp1->next;
-       
+
 //     }
 //     cout << "No Intersection Here\n";
 //     return false;
 // }
 
-// Efficient Way 
-int getIntersection(Node* head1,Node* head2){
-    Node* temp1 = head1;
-    Node* temp2 = head2;
+// Efficient Way
+// int getIntersection(Node* head1,Node* head2){
+//     Node* temp1 = head1;
+//     Node* temp2 = head2;
 
-    while(temp1 != temp2){
-        if(temp1 == NULL){
-            temp1 = head2;
-        } else {
-            temp1 = temp1->next;
-        }
-        if(temp2 == NULL){
-            temp2 = head1;
-        } else {
-            temp2 = temp2->next;
-        }
+//     while(temp1 != temp2){
+//         if(temp1 == NULL){
+//             temp1 = head2;
+//         } else {
+//             temp1 = temp1->next;
+//         }
+//         if(temp2 == NULL){
+//             temp2 = head1;
+//         } else {
+//             temp2 = temp2->next;
+//         }
 
+//     }
+//     return temp1->data;
+// }
+
+// Quest 2
+
+
+
+void printList(list<int> ll){
+    for (int x : ll){
+        cout << x << "->"; 
     }
-    return temp1->data;
+    cout << "NULL\n";
 }
-int main(){
-    Node* head1 = new Node(1);
-    head1->next = new Node(2);
-    head1->next->next = new Node(3);
-    Node* common = new Node(6);
-    common->next = new Node(7);
-    head1->next->next->next = common;
 
-    Node* head2 = new Node(4);
-    head2->next = new Node(5);
-    head2->next->next = common;
-    // cout << intersection(head1,head2);
-    cout << getIntersection(head1,head2);
+void deleteNNodes(list<int> ll,int n,int m){
+    list<int>::iterator temp = ll.begin();
+    list<int> ans;
+    while(temp != ll.end()){
+        int countM = m;
+        int countN = n;
+        
+        while(countM != 0){
+            if(temp == ll.end()){
+                break;
+            }
+            ans.push_back(*temp);
+            temp++;
+            countM--;
+        }
+        while (countN != 0)
+        {
+            if(temp == ll.end()){
+                break;
+            }
+            temp++;
+            countN--;
+        }
+    }
+
+    printList(ans);
+}
+
+
+int main()
+{
+    std::list<int> ll;
+    ll.push_back(1);
+    ll.push_back(2);
+    ll.push_back(3);
+    ll.push_back(4);
+    ll.push_back(5);
+    ll.push_back(6);
+    ll.push_back(7);
+    ll.push_back(8);
+    
+    int n  = 2;
+    int m  = 2;
+    deleteNNodes(ll,n,m);
 
     return 0;
 }

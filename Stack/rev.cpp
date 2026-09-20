03383f0a5c1c089ff6262b317237785a3f5b356f
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-09-20 13:02:54 +0530

    written its logic

--- Stack/rev.cpp
+++ Stack/rev.cpp
@@ -211,6 +211,21 @@ void nextGreaterElement(vector<int> arr,vector<int> ans){
     ans[idx] = -1;
     s.push(arr[idx]);
 
+    for(idx = idx-1; idx >=0 ; idx--){
+        int curr = arr[idx];
+        while(!s.empty() && curr >= s.top()){
+            s.pop();
+        }
+
+        if(s.empty()){
+            ans[idx] = -1;
+        } else{
+            ans[idx] = s.top();
+        }
+
+        s.push(curr);
+    }
+
     
 }
 

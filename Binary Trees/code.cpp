64817a10bc442f716d2fdd8bac4b5be1523f2409
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-09-20 13:32:25 +0530

    learnt about preorder traversals

--- Binary Trees/code.cpp
+++ Binary Trees/code.cpp
@@ -26,6 +26,11 @@ Node* buildTree(vector<int>nodes){
     return currNode;
 }
 
+// PreOrder Traversals
+void preorder(root){
+
+}
+
 int main(){
     vector<int>nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
     Node* root = buildTree(nodes);

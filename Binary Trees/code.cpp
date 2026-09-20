c89aaaac6e5711a7a9cc28db081a69d74161a151
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-09-20 13:47:59 +0530

    learnt about postorder traversal

--- Binary Trees/code.cpp
+++ Binary Trees/code.cpp
@@ -37,21 +37,33 @@ Node* buildTree(vector<int>nodes){
 // }
 
 // Inorder Traversal
-void inorder(Node* root){
+// void inorder(Node* root){
+//     if(root == NULL){
+//         return;
+//     }
+
+//     inorder(root->left);
+//     cout << root->data << " ";
+//     inorder(root->right);
+// }
+
+// PostOrder Traversal
+void postorder(Node* root){
     if(root == NULL){
         return;
     }
-
-    inorder(root->left);
+    postorder(root->left);
+    postorder(root->right);
     cout << root->data << " ";
-    inorder(root->right);
 }
+
 
 int main(){
     vector<int>nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
     Node* root = buildTree(nodes);
 
     // preorder(root);
-    inorder(root);
+    // inorder(root);
+    postorder(root);
     return 0;
 }

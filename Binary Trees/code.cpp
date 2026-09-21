21e6bac259e5344b5648fde019bed87360c870be
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-09-21 12:34:43 +0530

    written the logic and understood

--- Binary Trees/code.cpp
+++ Binary Trees/code.cpp
@@ -92,6 +92,16 @@ Node* buildTree(vector<int>nodes){
 // }
 
 // Height of Tree
+int height(Node* root){
+    if(root == NULL){
+        return 0;
+    }
+    int leftHt = height(root->left);
+    int rightHt = height(root->right);
+
+    int currHt = max(leftHt,rightHt) + 1;
+    return currHt;
+}
 
 int main(){
     vector<int>nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};

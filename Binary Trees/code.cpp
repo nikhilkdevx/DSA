cf5d6069310ccb64b29391e1b536c6e662595306
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-09-23 12:31:43 +0530

    leraning about counting Nodes logic

--- Binary Trees/code.cpp
+++ Binary Trees/code.cpp
@@ -104,6 +104,14 @@ int height(Node* root){
     return currHt;
 }
 
+// Count of Nodes
+int countNode(Node* root){
+    
+
+
+}
+
+
 int main(){
     vector<int>nodes = {1,2,4,-1,-1,5,-1,6,-1,7,-1,-1,3,-1,-1};
     Node* root = buildTree(nodes);

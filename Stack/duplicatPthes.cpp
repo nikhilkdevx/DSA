f1bbbf5de71fb868fcb98ca51d501020d41fa672
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-09-23 12:16:18 +0530

    written the logic

--- Stack/duplicatPthes.cpp
+++ Stack/duplicatPthes.cpp
@@ -1,28 +1,38 @@
-#include<iostream>
-#include<vector>
-#include<stack>
+#include <iostream>
+#include <vector>
+#include <stack>
 using namespace std;
 
-bool isDuplicate(string str){
-    stack <char> s;
-    for(int i = 0;i<str.size();i++){
+bool isDuplicate(string str)
+{
+    stack<char> s;
+    for (int i = 0; i < str.size(); i++)
+    {
         char ch = str[i];
-        if(ch != ')'){
+
+        if (ch != ')')
+        {
             s.push(ch);
-        } else {
-           if(s.top() == '('){
-            return true ;
-           } 
-           while(s.top() != '('){
+        }
+        else
+        {
+            if (s.top() == '(')
+            {
+                return true;
+            }
+            while (s.top() != '(')
+            {
+                s.pop();
+            }
             s.pop();
-           }
-           s.pop();
         }
     }
+
     return false;
 };
 
-int main(){
+int main()
+{
     string str1 = "((a+b))";
     string str2 = "((a+b) + (c+d))";
     cout << isDuplicate(str1) << endl;
--- Stack/rev.cpp
+++ Stack/rev.cpp
@@ -278,15 +278,31 @@ using namespace std;
 
 // duplicat pthes
 bool duplicatPthes(string str){
-    stack<int>s;
+    stack<char>s;
     for(int i=0;i<str.size();i++){
-        
+        char ch = str[i];
+        if(ch != ')'){
+            s.push(ch);
+        } else {
+            if(s.top() == '('){
+                return true;
+            }
+            while(s.top() != '('){
+                s.pop();
+            }
+            s.pop();
+        }
     }
+
+    return false;
 }
 
 int main()
 {
     string str1 = "((a+b) + (c+d))";
-    string str1 = "(((a+b) + (c+d)))";
+    string str2 = "(((a+b) + (c+d)))";
+    cout << duplicatPthes(str1);
+    cout << duplicatPthes(str2);
+
     return 0;
 }

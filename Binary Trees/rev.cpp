c8d5534b8f7cd53714f563517823d874704bf319
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-09-24 13:17:12 +0530

    written postorder logic

--- Binary Trees/rev.cpp
+++ Binary Trees/rev.cpp
@@ -46,9 +46,19 @@ void inorder(Node* root){
     inorder(root->right);
 }
 
+void postorder(Node* root){
+    if(root == NULL){
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
 int main(){
 vector<int> nodes = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
 Node* root = buildTree(nodes);
 // preOrder(root);
-inorder(root);
+// inorder(root);
+// postorder(root);
 }

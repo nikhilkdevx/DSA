c503f6568be767443fe64ec48f764bc93478e7eb
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-09-24 13:10:44 +0530

    written preorder logic

--- Binary Trees/rev.cpp
+++ Binary Trees/rev.cpp
@@ -28,9 +28,18 @@ Node* buildTree(vector<int>nodes){
 }
 
 // Traversals
+void preOrder(Node* root){
+    if(root == NULL){
+        return ;
+    }
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
 
 
 int main(){
 vector<int> nodes = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
 Node* root = buildTree(nodes);
+preOrder(root);
 }

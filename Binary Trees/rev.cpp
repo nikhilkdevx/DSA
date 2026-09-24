efd2d5380345ed6e488bf0de08e542b8902c5dd4
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-09-24 12:47:26 +0530

    written the boilerplate logic and defined class Node for the tree

--- Binary Trees/rev.cpp
+++ Binary Trees/rev.cpp
@@ -2,3 +2,21 @@
 #include<vector>
 using namespace std;
 
+class Node{
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        this->data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+
+int main(){
+vector<int> nodes = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+Node* root = buildTree(nodes);
+}

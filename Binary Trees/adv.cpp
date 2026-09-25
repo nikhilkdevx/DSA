2a229acbcfea836fdb439f50ba5bc014433e4f76
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-09-25 12:46:09 +0530

    intializing the code

--- Binary Trees/adv.cpp
+++ Binary Trees/adv.cpp
@@ -1,5 +1,12 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-// Diameter of a Tree
 
+
+int main(){
+    vector<int> nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+    Node* root = buildTree(nodes);
+    // cout << "Diameter = " << diam1(root);
+    return 0;
+};

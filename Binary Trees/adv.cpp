d6150005e5b490d8651ceca62a514e35f2050b3e
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-09-25 12:46:40 +0530

    intialized node class

--- Binary Trees/adv.cpp
+++ Binary Trees/adv.cpp
@@ -2,6 +2,18 @@
 #include<vector>
 using namespace std;
 
+class Node{
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data){
+        this->data = data;
+        left = right = NULL; 
+    }
+};
+
 
 
 int main(){

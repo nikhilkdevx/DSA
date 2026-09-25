0ce14ae3f636735dfa9f29827127e488909157c4
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-09-25 12:48:14 +0530

    writing the logic of finding diameter of the tree  func

--- Binary Trees/adv.cpp
+++ Binary Trees/adv.cpp
@@ -36,7 +36,11 @@ int heightOfTree(Node* root){
     int currHt = max(leftHt,rightHt) + 1;
     return currHt;
 }
+// Diameter of a Tree
+int diam1(Node* root){ // [O(n^2)]
+    
 
+}
 
 int main(){
     vector<int> nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};

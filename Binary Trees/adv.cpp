c2677733c0edb26c0f77da6e2d792592d41cfe6d
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-09-26 12:54:28 +0530

    optimising curr soln

--- Binary Trees/adv.cpp
+++ Binary Trees/adv.cpp
@@ -49,6 +49,14 @@ int diam1(Node* root){ // [O(n^2)]
 
 }
 
+// Diameter of Tree [O(n)]
+int diam2(Node* root,int height){
+    if(root == NULL){
+        return 0;
+    }
+    
+}
+
 int main(){
     vector<int> nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
     Node* root = buildTree(nodes);

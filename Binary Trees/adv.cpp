ea2c66f568e807c446db76de6d9139f5b265de28
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-09-26 13:06:25 +0530

    logic working

--- Binary Trees/adv.cpp
+++ Binary Trees/adv.cpp
@@ -66,6 +66,6 @@ pair<int,int> diam2(Node* root){
 int main(){
     vector<int> nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
     Node* root = buildTree(nodes);
-    cout << "Diameter :  " << diam1(root);
+    cout << " Diameter :  " << diam2(root).first;
     return 0;
 };
